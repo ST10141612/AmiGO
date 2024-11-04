0366bc3a7f914fe3edfce64f07798d28c3c07313
ST10141612 <st10141612@vcconnect.edu.za>
2024-11-04 11:02:40 +0200

    Final POE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "mtm.example.amigo"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
